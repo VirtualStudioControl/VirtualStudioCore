18b0ba6745c650bc2c6bd2327301b3274ab869f8
ebosseck <ebosseck@lunznet.de>
2021-07-09 01:36:29 +0200

    Added Hardware Parameter API

--- virtualstudio/core/devicemanager/hardware/elgato/elgato_streamdeck_wrapper.py
+++ virtualstudio/core/devicemanager/hardware/elgato/elgato_streamdeck_wrapper.py
@@ -2,14 +2,38 @@ from virtualstudio.common.structs.hardware.hardware_wrapper import *
 
 from streamdeck.devices.streamdeck import StreamDeck
 
+
+STREAMDECK_ICON_RESOLUTION = {
+    "Stream Deck Mini": (80, 80),
+    "Stream Deck Original": (72, 72),
+    "Stream Deck Original (V2)": (72, 72),
+    "Stream Deck XL": (96, 96),
+}
+
+
+STREAMDECK_ICON_FORMAT = {
+    "Stream Deck Mini": "BMP",
+    "Stream Deck Original": "BMP",
+    "Stream Deck Original (V2)": "JPEG",
+    "Stream Deck XL": "JPEG",
+}
+
+
 class StreamdeckDeviceWrapper(HardwareWrapper):
 
     def __init__(self, device: StreamDeck):
         super().__init__(device, device.id(), device.deck_type(), "Elgato")
+
+    def getHardwareParameters(self) -> Optional[Dict]:
+        parameters = {}
+
+        parameters["icon_resolution"] = STREAMDECK_ICON_RESOLUTION[self.name]
+        parameters["icon_format"] = STREAMDECK_ICON_FORMAT[self.name]
+
+        return parameters
 
     def getType(self):
         return HARDWARE_TYPE_ELGATO
 
     def bindProfile(self, profile):
         self.currentProfile = profile.name
-        pass
